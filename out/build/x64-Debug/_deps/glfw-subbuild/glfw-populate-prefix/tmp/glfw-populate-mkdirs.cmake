7e2b4536e201702beeefc65e4362df625ac0a15c
Wiesiek <jakubwisniewski1h@gmail.com>
2025-04-10 12:14:55 +0200

    Update

--- out/build/x64-Debug/_deps/glfw-subbuild/glfw-populate-prefix/tmp/glfw-populate-mkdirs.cmake
+++ out/build/x64-Debug/_deps/glfw-subbuild/glfw-populate-prefix/tmp/glfw-populate-mkdirs.cmake
@@ -3,8 +3,13 @@
 
 cmake_minimum_required(VERSION 3.5)
 
+# If CMAKE_DISABLE_SOURCE_CHANGES is set to true and the source directory is an
+# existing directory in our source tree, calling file(MAKE_DIRECTORY) on it
+# would cause a fatal error, even though it would be a no-op.
+if(NOT EXISTS "D:/Projekty/Vulkan/OpenGL Projekt/out/build/x64-Debug/_deps/glfw-src")
+  file(MAKE_DIRECTORY "D:/Projekty/Vulkan/OpenGL Projekt/out/build/x64-Debug/_deps/glfw-src")
+endif()
 file(MAKE_DIRECTORY
-  "D:/Projekty/Vulkan/OpenGL Projekt/out/build/x64-Debug/_deps/glfw-src"
   "D:/Projekty/Vulkan/OpenGL Projekt/out/build/x64-Debug/_deps/glfw-build"
   "D:/Projekty/Vulkan/OpenGL Projekt/out/build/x64-Debug/_deps/glfw-subbuild/glfw-populate-prefix"
   "D:/Projekty/Vulkan/OpenGL Projekt/out/build/x64-Debug/_deps/glfw-subbuild/glfw-populate-prefix/tmp"
@@ -17,3 +22,6 @@ set(configSubDirs )
 foreach(subDir IN LISTS configSubDirs)
     file(MAKE_DIRECTORY "D:/Projekty/Vulkan/OpenGL Projekt/out/build/x64-Debug/_deps/glfw-subbuild/glfw-populate-prefix/src/glfw-populate-stamp/${subDir}")
 endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "D:/Projekty/Vulkan/OpenGL Projekt/out/build/x64-Debug/_deps/glfw-subbuild/glfw-populate-prefix/src/glfw-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
